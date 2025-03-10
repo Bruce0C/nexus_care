5471acbf02c35e0cc51dc10936d3b07cb04f77b2
Bruce Chibisa <pvlm5xi2@students.codeinstitute.net>
2025-03-10 19:19:37 +0000

    Add function to collect user name

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import gspread
-from google.oauth2.service_account import Credentials 
+from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +12,19 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('nexuscare')
 
+user = SHEET.worksheet('user')
 home = SHEET.worksheet('home')
+medication = SHEET.worksheet('medication')
+schedule = SHEET.worksheet('schedule')
 
-data = home.get_all_values()
 
-print(data)
+def user_name():
+    """
+    Enter and log user name
+    """
+
+
+name = input('Please enter your name here: ')
+print(f'Hello {name}')
+
+user_name()
